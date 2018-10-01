8acbd067f4b0428d1055caa91ff4684699f92509
Vedran Knezevic <vedranknezevic1@yahoo.com>
2018-10-01 15:57:25 +0200

    Init commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,45 @@
+//const MongoClient = require("mongodb").MongoClient;
+
+const { MongoClient, ObjectID } = require("mongodb");
+
+MongoClient.connect(
+  "mongodb://localhost:27017/TodoApp",
+  (err, db) => {
+    if (err) {
+      return console.log("Unable to connect to MongoDB server");
+    }
+
+    console.log("Connected to MongoDB server");
+
+    // db.collection("Todos").insertOne(
+    //   {
+    //     text: "Something to do",
+    //     completed: false
+    //   },
+    //   (err, result) => {
+    //     if (err) {
+    //       return console.log("Unable to inser todo", err);
+    //     }
+
+    //     console.log(JSON.stringify(result.ops, undefined, 2));
+    //   }
+    // );
+
+    // db.collection("Users").insertOne(
+    //   {
+    //     name: "Vedran",
+    //     age: 31,
+    //     location: "Zagreb"
+    //   },
+    //   (err, result) => {
+    //     if (err) {
+    //       return console.log("Unable to insert Users", err);
+    //     }
+
+    //     console.log(JSON.stringify(result.ops[0]._id.getTimestamp()));
+    //   }
+    // );
+
+    db.close();
+  }
+);
