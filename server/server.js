b6aee67d7ae6d019e59495265630b1759811fa7c
Vedran Knezevic <vedranknezevic1@yahoo.com>
2018-10-02 15:29:35 +0200

    Add GET / todos / :ID

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { ObjectID } = require("mongodb");
 
 const { mongoose } = require("./db/mongoose");
 const { Todo } = require("./models/todo");
@@ -33,6 +34,28 @@ app.get("/todos", (req, res) => {
       res.status(400).send(e);
     }
   );
+});
+
+//GET /todos/123322
+app.get("/todos/:id", (req, res) => {
+  const id = req.params.id;
+
+  //validate id using isValid
+  //404
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send();
+  }
+
+  //findByID
+  Todo.findById(id)
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+
+      res.send({ todo });
+    })
+    .catch(e => res.status(400).send());
 });
 
 app.listen(3000, () => {
