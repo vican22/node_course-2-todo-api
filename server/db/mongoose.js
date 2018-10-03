b6436636a5057435f2c8d80743ca0b7f4c30652b
Vedran Knezevic <vedranknezevic1@yahoo.com>
2018-10-03 10:08:52 +0200

    New push

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,12 +3,24 @@ const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 //mongoose.connect("mongodb://localhost:27017/TodoApp");
 
+// let connectionString =
+//   process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp";
+// connectionString =
+//   "mongodb://vican22:qwertz6@ds121673.mlab.com:21673/todo-app-api";
+
+// mongoose
+//   .connect(connectionString)
+//   .then(() => {
+//     console.log(`connected to ${connectionString}`);
+//   })
+//   .catch(err => console.log(`error connecting: ${err}`));
+
 let db = {
   localhost: "mongodb://localhost:27017/TodoApp",
-  mlab: "mongodb://<vican22>:<123456v>@ds121673.mlab.com:21673/todo-app-api"
+  mlab: "mongodb://vedran:qwertz6@ds121673.mlab.com:21673/todo-app-api"
 };
 
-mongoose.connect(db.localhost || db.mlab);
+mongoose.connect(process.env.PORT ? db.localhost : db.mlab);
 module.exports = {
   mongoose
 };
